b0f49f48ccccb0f439edfa1112a4b88573a7942d
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-09-02 18:26:56 -0300

    config: hex package metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Atproto.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/zoedsoupe/atproto-ex"
+
   def project do
     [
       app: :atproto,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -22,8 +28,35 @@ defmodule Atproto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", runtime: false, only: [:dev, :prod]},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
+  end
+
+  defp package do
+    %{
+      name: "atproto_http",
+      licenses: ["MIT"],
+      contributors: ["zoedsoupe"],
+      links: %{
+        "GitHub" => @source_url,
+        "Docs" => "https://hexdocs.pm/atproto"
+      },
+      files: ~w[lib mix.exs README.md LICENSE]
+    }
+  end
+
+  defp docs do
+    [
+      main: "Atproto",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp description do
+    """
+    ATProtocol and Bluesky client for Elixir (using HTTP)
+    """
   end
 end
